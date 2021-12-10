d1623bd3846defd81386e56c47a2f992f6791e0d
JosAnGoCa <josetono612@gmail.com>
2021-12-09 18:38:01 -0600

    Funicones y proyecto listo para trabajar - JosAnGoCa

--- Proyecto-Final/Proyecto-Final/Proyecto-Final.cpp
+++ Proyecto-Final/Proyecto-Final/Proyecto-Final.cpp
@@ -3,6 +3,25 @@
 int main()
 {
     // Proyecto final de introduccion a la ingenieria en computacion
+    // Dia limite para subir sus librerias 13/12/2021
 
-    suma(numero1, numero2);
+    int numero1, numero2;
+
+    printf("Primer numero: ")
+    scanf_s("%i", numero1);
+
+    printf("Segundo numero: ")
+    scanf_s("%i", numero2);
+
+    // David
+    // Suma
+
+    // Bryan
+    // Resta
+
+    // Salazar
+    // Multiplicacion
+
+    // Daniel
+    // Modulo
 }
